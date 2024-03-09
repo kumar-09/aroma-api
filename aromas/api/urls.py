0e393b50ab545625c13cb74525ef192859f672f4
Ayushmaan <76957103+j1ntan@users.noreply.github.com>
2024-03-09 19:49:47 +0530

    CREATE: created login api

--- aromas/api/urls.py
+++ aromas/api/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('menu/',views.getmenu, name='Menu'),
     path('register/',views.register),
+    path('login/',views.login, name='Login'),
     path('menu/<str:type>/',views.Category, name='Category'),
     path('additem/',views.additem,name='NEWITEM'),
     path('prevlist/<str:pk>',views.PreviousOrders,name='LastOrders'),
